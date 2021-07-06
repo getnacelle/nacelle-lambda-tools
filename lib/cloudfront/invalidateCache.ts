e8898b7dbb66e25df07c3d168e0ed598523ec932
irnoble <irnoble59@gmail.com>
2021-07-06 11:22:37 -0400

    remove consolelogs

--- lib/cloudfront/invalidateCache.ts
+++ lib/cloudfront/invalidateCache.ts
@@ -18,7 +18,6 @@ function init(cloudfrontInstance: CloudFront): CloudFrontTools {
       quantity: number,
       paths: string[]
     ): Promise<PromiseResult<CloudFront.CreateInvalidationResult, AWSError>> {
-      console.log('starting.....')
       const currentTimestamp = new Date().getTime()
       const result = await cloudfrontInstance.createInvalidation({
         DistributionId: distributionId,
@@ -31,10 +30,7 @@ function init(cloudfrontInstance: CloudFront): CloudFrontTools {
         }
       })
         .promise()
-      console.log(`result________________`)
-      console.log(JSON.stringify(result))
-      console.log(`result.Invalidation________________`)
-      console.log(JSON.stringify(result.Invalidation))
+
       logger.debug(`Successfully cleared cache for cloudfront distribution: ${distributionId} (paths: ${JSON.stringify(paths)})`)
 
       return result
